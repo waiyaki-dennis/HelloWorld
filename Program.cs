bf6265cc24e758bf6c46beb72188f36b86723c0a
waiyaki-dennis <waiyakidennis05@gmail.com>
2026-05-25 12:50:30 +0300

    Day 1 - Variables and data types

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string name = "Dennis";
+int age = 25;
+double salary = 150000.50;
+bool isLearning = true;
+char myGrade = 'A';
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Target Salary: " + salary);
+Console.WriteLine("Currently Learning: " + isLearning);
+Console.WriteLine("my target grade:" + myGrade );
